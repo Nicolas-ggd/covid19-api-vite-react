818befaf2e6edc000a925ffe251a087bb935ec9d
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-07 23:54:42 +0400

    added Header component && mui library for icons (#12)

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import { Login } from "./Login";
 import { Home } from "./Home";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Header } from "./components/header/Header";
 
 function App() {
   return (
-    <>
+    <div className="w-screen h-screen bg-gray-50">
       <Router>
+      <Header />
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
           <Route exact path="/login" element={<Login />}></Route>
         </Routes>
-        <div>
-          <button class="rounded-none bg-cyan-400">Save Changes</button>
-          <Link to="/">Home</Link>
-          <Link to="/login">Login</Link>
-        </div>
       </Router>
-    </>
+    </div>
   )
 }
 
