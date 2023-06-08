7ef4b6941ffeaed3be4290d9da0dbb863857a7a4
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-08 23:01:02 +0400

    validation in login (#6)

--- src/components/authorization/SignIn.jsx
+++ src/components/authorization/SignIn.jsx
@@ -1,5 +1,29 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export const SignIn = ({ closeSignIn }) => {
+    const [isError, setIsError] = useState(false);
+    const [signInData, setSignInData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const submitSignInData = async (event) => {
+        if (signInData?.email?.length === 0 || signInData?.password?.length === 0) {
+            return setIsError(true);
+        }
+        event.preventDefault();
+
+        await axios.post("http://localhost:8000/auth", {
+            email: signInData.email,
+            password: signInData.password,
+        })
+            .then((res) => {
+                const data = res.data;
+                console.log(data);
+            });
+    };
+
     return (
         <section className="bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -12,15 +36,47 @@ export const SignIn = ({ closeSignIn }) => {
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Sign in to your account
                         </h1>
-                        <form className="space-y-4 md:space-y-6" action="#">
+                        <form className="space-y-4 md:space-y-6" onSubmit={submitSignInData}>
                             <div>
                                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                                <input
+                                    style={{ borderColor: isError && signInData?.email?.length === 0 ? 'red' : '' }}
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none"
+                                    placeholder="name@company.com"
+                                    required=""
+                                    onChange={(e) => {
+                                        setSignInData((prevSendData) => ({
+                                            ...prevSendData,
+                                            email: e.target.value
+                                        }));
+                                    }}
+                                />
                             </div>
+                            {isError && signInData?.email?.length <= 0 && <span style={{ color: "red", margin: "3px" }}>Please fill the email</span>}
+
                             <div>
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                <input
+                                    style={{ borderColor: isError && signInData?.password?.length === 0 ? 'red' : '' }}
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    placeholder="••••••••"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none"
+                                    required=""
+                                    onChange={(e) => {
+                                        setSignInData((prevSendData) => ({
+                                            ...prevSendData,
+                                            password: e.target.value
+                                        }));
+                                    }}
+                                />
                             </div>
+                            {isError && signInData?.password?.length === 0 && <span style={{ color: "red", margin: "3px" }}>Please fill the passowrd</span>}
+
                             <div className="flex items-center justify-between">
                                 <div className="flex items-start">
                                     <div className="flex items-center h-5">
@@ -32,9 +88,10 @@ export const SignIn = ({ closeSignIn }) => {
                                 </div>
                                 <a className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                             </div>
-                            <button type="submit" className="w-full text-white bg-sky-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+
+                            <button onClick={submitSignInData} type="button" className="w-full text-white bg-sky-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 outline-none">Sign in</button>
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Dont have an account yet? <a onClick={closeSignIn} className="font-medium text-primary-600 hover:underline dark:text-primary-500 cursor-pointer">Sign up</a>
+                                Dont have an account yet? <a onClick={closeSignIn} className="font-medium text-primary-600 hover:underline dark:text-primary-500 cursor-pointer outline-none">Sign up</a>
                             </p>
                         </form>
                     </div>
