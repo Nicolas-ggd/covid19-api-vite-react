50cb1512db8a986fe2bc00f92d86623c779f2233
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-11 17:39:19 +0400

    fix login bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { PrivateRoutes } from "./utils/PrivateRoutes";
-import { SignIn } from "./components/authorization/SignIn";
 import { Header } from "./components/header/Header";
 import { Home } from "./components/home/Home";
+import { Auth } from "./components/auth/Auth";
 
 function App() {
   return (
     <div className="w-screen h-screen bg-gray-50">
       <Router>
-            {/* <Header /> */}
+        <Header />
         <Routes>
           <Route element={<PrivateRoutes />}>
             <Route element={<Home />} exact path="/home" ></Route>
           </Route>
-          <Route exact path="/" element={<SignIn />}></Route>
+          <Route exact path="/" element={<Auth />}></Route>
         </Routes>
 
       </Router>
