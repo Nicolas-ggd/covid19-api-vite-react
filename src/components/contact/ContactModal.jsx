7bf9166d0911a10bac9e4e1da6ac6560b83f9f0d
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-26 00:13:29 +0400

    send contact me information

--- src/components/contact/ContactModal.jsx
+++ src/components/contact/ContactModal.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
+import axios from "axios";
 
 export const ContactModal = ({ onClose }) => {
     const [isContact, setIsContact] = useState({
+        email: '',
         contactType: '',
         text: ''
     })
-    
+
+    const sendContactInfo = async () => {
+        await axios.post('http://localhost:8000/contact/create-contact', {
+            contactType: isContact?.contactType,
+            text: isContact?.text
+        })
+    };
+
     return (
         <div className="absolute flex align-center justify-center top-0 left-0 right-0 z-50 w-full h-full p-4" style={{ background: '#00000080' }}>
             <div className="relative w-full max-w-md max-h-full py-80">
@@ -16,7 +25,23 @@ export const ContactModal = ({ onClose }) => {
                     </button>
                     <div className="px-6 py-6 lg:px-8">
                         <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Write Text</h3>
-                        <form className="space-y-6">
+                        <form className="space-y-6" onSubmit={sendContactInfo}>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter your email</label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    placeholder="example@gmail.com"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                    required
+                                    onChange={(e) => {
+                                        setIsContact((prevData) => ({
+                                            ...prevData,
+                                            email: e.target.value
+                                        }));
+                                    }}
+                                />
+                            </div>
                             <div>
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contact Type</label>
                                 <input
@@ -49,7 +74,7 @@ export const ContactModal = ({ onClose }) => {
                                     }}
                                 />
                             </div>
-                            <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send</button>
+                            <button type="button" onClick={sendContactInfo} className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send</button>
                         </form>
                     </div>
                 </div>
