66f670bfd83abd46e86eb40095f90ad5ce81e1d2
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-25 22:49:00 +0400

    feature: add dark mode in contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,12 @@
 
-export const Contact = () => {
+export const Contact = ({ themeClass, isDarkMode }) => {
     return (
-        <div className="w-full">
-            <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
+        <div className={`w-full ${themeClass}`}>
+            <div className={`grid grid-cols-1 md:grid-cols-2 h-screen`}>
                 <div className="max-h-96 md:h-screen">
                     <img className="w-screen h-screen object-cover object-top" src="https://images.pexels.com/photos/270373/pexels-photo-270373.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
                 </div>
-                <div className="flex bg-gray-100 p-10">
+                <div className={`flex bg-gray-100 p-10 ${isDarkMode ? 'bg-gray-800' : 'bg-white'}`}>
                     <div className="mb-auto mt-auto max-w-lg">
                         <h1 className="text-3xl">Nicolas Ggd</h1>
                         <p className="font-semibold mb-5">Web Developer</p>
