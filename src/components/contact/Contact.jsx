37b80939accfd39b130afb94028ce9d67a10ee38
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-25 23:25:35 +0400

    feature: contact modal

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
+import { ContactModal } from "./ContactModal";
 
 export const Contact = ({ themeClass, isDarkMode }) => {
+    const [isClick, setIsClick] = useState(false);
+
+    const contactEmailHandler = () => {
+        setIsClick(prevData => !prevData);
+    };
+
     return (
-        <div className={`w-full ${themeClass}`}>
+        <div className={`w-full h-full ${themeClass}`}>
             <div className={`grid grid-cols-1 md:grid-cols-2 h-screen`}>
                 <div className="max-h-96 md:h-screen">
                     <img className="w-screen h-screen object-cover object-top" src="https://images.pexels.com/photos/270373/pexels-photo-270373.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -11,10 +20,11 @@ export const Contact = ({ themeClass, isDarkMode }) => {
                         <h1 className="text-3xl">Nicolas Ggd</h1>
                         <p className="font-semibold mb-5">Web Developer</p>
                         <p>Never say never, because the limits like fears are often just an illusion.</p>
-                        <button className="bg-black rounded-md py-3 px-7 mt-6 text-white">Email Me</button>
+                        <button onClick={contactEmailHandler} className="bg-black rounded-md py-3 px-7 mt-6 text-white">Email Me</button>
                     </div>
                 </div>
             </div>
+            {isClick && <ContactModal onClose={contactEmailHandler} />}
         </div>
     )
 };
