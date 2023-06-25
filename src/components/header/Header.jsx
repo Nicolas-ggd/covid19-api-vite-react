4cb3e0bcebd07a2193d67121fc21a152e004c69b
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-25 22:43:53 +0400

    modify header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import LiveTvIcon from '@mui/icons-material/LiveTv';
 
-export const MainHeader = ({ themeClass, isDarkMode }) => {
+export const MainHeader = ({ themeClassName, isDarkMode }) => {
     const navigate = useNavigate();
     const [searchValue, setSearchValue] = useState('');
 
@@ -27,50 +27,40 @@ export const MainHeader = ({ themeClass, isDarkMode }) => {
     };
     return (
         <>
-            <nav className={`border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 ${themeClass} ${isDarkMode ? 'bg-gray-600' : 'bg-white'}`}>
-                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <div className="flex items-center lg:order-2">
-                        <div className="flex items-center">
-                            <LogoutIcon />
-                            <a onClick={userLogOut} className={`hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800 ${isDarkMode ? 'text-white ' : 'text-gray-800'}`}>Log Out</a>
+            <header>
+                <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+                    <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                        <a href="https://flowbite.com" className="flex items-center">
+                            <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+                            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                        </a>
+                        <div className="flex items-center lg:order-2">
+                            <a onClick={userLogOut} className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log Out</a>
+                            <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                                <span className="sr-only">Open main menu</span>
+                                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+                                <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                            </button>
                         </div>
-
+                        <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+                            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                                <li>
+                                    <Link to="/home" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/movies" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Movies</Link>
+                                </li>
+                                <li>
+                                    <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Features</a>
+                                </li>
+                                <li>
+                                    <Link to="/contact" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Contact</Link>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-
-                    <div className="w-full min-w-0 lg:px-6 xl:w-3/4 xl:px-12">
-                        <form onSubmit={handleSearchSubmit}>
-                            <div className="relative">
-                                <input
-                                    type="text"
-                                    className="transition-colors duration-100 ease-in-out text-gray-600 py-2 pr-4 pl-10 block w-full appearance-none leading-normal border border-transparent rounded-lg text-left select-none truncate bg-gray-200"
-                                    value={searchValue}
-                                    onChange={handleSearchChange}
-                                    placeholder="Search Movies"
-                                />
-                                <div className="absolute inset-y-0 left-0 pl-4 flex items-center">
-                                    <svg className="fill-current text-gray-600 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                        <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
-                                    </svg>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            <li className="flex">
-                                <HomeOutlinedIcon />
-                                <Link to="/home" className={`block ml-1 py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700 ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>Home</Link>
-                            </li>
-                            <li className="flex">
-                                <LiveTvIcon />
-                                <Link to="/movies" className={`block ml-1 py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700 ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>Movies</Link>
-                            </li>
-                        </ul>
-                    </div>
-
-
-                </div>
-            </nav>
+                </nav>
+            </header>
         </>
     );
 };
