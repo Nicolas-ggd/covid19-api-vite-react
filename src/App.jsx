f09ac7317feb060a36bb2f742d89934a020c15ee
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-25 22:43:20 +0400

    feature: add new route and component

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,8 @@ import { ForgotPassword } from "./components/forgotPassword/ForgotPassword";
 import { SignIn } from "./components/authorization/SignIn";
 import { Page404 } from "./components/nonExist/404Page";
 import { VideoPlayer } from "./components/videoPlayer/VideoPlayer";
-// import io from 'socket.io-client';
+import { Contact } from "./components/contact/Contact";
 
-// io.connect('http://localhost:8000')
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
@@ -33,6 +32,7 @@ function App() {
           <Route element={<PrivateRoutes />}>
             <Route element={<Home themeClass={themeClass} isDarkMode={isDarkMode} />} exact path="/home"></Route>
             <Route exact path="/movies" element={<VideoPlayer width="100%" height="600px" />}></Route>
+            <Route exact path="/contact" element={<Contact themeClass={themeClass} isDarkMode={isDarkMode} />}></Route>
           </Route>
           <Route exact path="/" element={<Auth themeClass={themeClass} isDarkMode={isDarkMode} />}></Route>
           <Route exact path="/verify" element={<SignIn />}></Route>
