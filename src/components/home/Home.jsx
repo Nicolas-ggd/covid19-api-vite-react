6e168c3adfc3a38a9b8e89a1e618ec61b8380511
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-27 01:36:51 +0400

    add another skeleton card for user list

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -45,13 +45,24 @@ export const Home = ({ themeClass, isDarkMode }) => {
 
       <div className="px-10 flex justify-between">
         <div className="pt-10">
-          <div className={`max-w-sm p-6 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ${isDarkMode ? 'bg-grey-800 border-gray-700' : 'bg-white'}`}>
+          {isUserList && <div className={`max-w-sm p-6 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ${isDarkMode ? 'bg-grey-800 border-gray-700' : 'bg-white'}`}>
             <GroupIcon style={{ fontSize: '35px' }} className={`${isDarkMode ? 'text-white' : 'text-grey-900'}`} />
             <a href="#">
               <h5 className={`mb-2 text-2xl font-semibold tracking-tight ${isDarkMode ? 'text-white' : 'text-gray-900'}`}>Need a help in Claim?</h5>
             </a>
             <p className={`mb-3 font-normal text-gray-500 dark:text-gray-400 ${isDarkMode ? 'text-white' : 'text-grey-900'}`}>Total Users: {isUserList?.length}</p>
-          </div>
+          </div>}
+          {!isUserList && (
+            <div role="status" class="max-w-sm animate-pulse">
+              <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+              <span class="sr-only">Loading...</span>
+            </div>
+          )}
         </div>
 
         <div className="pt-10">
@@ -67,6 +78,7 @@ export const Home = ({ themeClass, isDarkMode }) => {
 
             <div className="flow-root">
               <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
+
                 {!onlineUserData && (
                   <div role="status" class="max-w-sm animate-pulse">
                     <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
@@ -78,6 +90,7 @@ export const Home = ({ themeClass, isDarkMode }) => {
                     <span class="sr-only">Loading...</span>
                   </div>
                 )}
+
                 {onlineUserData && onlineUserData?.map((item, index) => {
                   return (
                     <li className="py-3 sm:py-4" key={index}>
