7256f30f3e3518c70abb40d3410a70167c77c363
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-29 02:57:16 +0400

    add filter in table

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,6 +9,7 @@ export const Home = ({ themeClass, isDarkMode }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
+  const [filterValue, setFilterValue] = useState('');
   const itemsPerPage = 15;
 
   useEffect(() => {
@@ -42,6 +43,14 @@ export const Home = ({ themeClass, isDarkMode }) => {
     setCurrentPage((prevPage) => prevPage - 1);
   };
 
+  const handleFilterChange = (e) => {
+    setFilterValue(e.target.value);
+  };
+
+  const filteredData = isCovidData?.filter((country) =>
+    country.country.toLowerCase().includes(filterValue.toLowerCase())
+  );
+
   return (
     <div className={`w-full h-full bg-grey-50 ${themeClass}`}>
       <MainHeader themeClass={themeClass} isDarkMode={isDarkMode} />
@@ -51,8 +60,8 @@ export const Home = ({ themeClass, isDarkMode }) => {
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
               <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
             </div>
-            <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
-              <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+            <input type="text" value={filterValue} onChange={handleFilterChange} id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+            <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
           </div>
         </form>
         <div className="mt-20 pt-10 w-full h-full relative overflow-x-auto shadow-md sm:rounded-lg">
@@ -89,7 +98,7 @@ export const Home = ({ themeClass, isDarkMode }) => {
               </tr>
             </thead>
             <tbody>
-              {isCovidData && isCovidData?.map((item, index) => {
+              {isCovidData && filteredData?.map((item, index) => {
                 return (
                   <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700" key={index}>
                     <th scope="row" className="flex items-center px-3 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
