a883181a7ff5da3ad3660ccd139a92b1a34aee75
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-29 02:50:06 +0400

    add search input

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -46,7 +46,16 @@ export const Home = ({ themeClass, isDarkMode }) => {
     <div className={`w-full h-full bg-grey-50 ${themeClass}`}>
       <MainHeader themeClass={themeClass} isDarkMode={isDarkMode} />
       <div className="px-10 flex justify-between">
-        <div className="pt-10 w-full h-full relative overflow-x-auto shadow-md sm:rounded-lg">
+        <form className="absolute my-5 w-80">
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+            </div>
+            <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+              <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+          </div>
+        </form>
+        <div className="mt-20 pt-10 w-full h-full relative overflow-x-auto shadow-md sm:rounded-lg">
           {isLoading && <div role="status">
             <div className="flex flex-col items-center justify-center h-screen w-screen">
               <svg aria-hidden="true" className="w-12 h-12 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
