fab05a1e56bb89006b140360e93002902ae6f07b
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-29 02:24:28 +0400

    update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,121 +1,112 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
-import GroupIcon from '@mui/icons-material/Group';
 import { MainHeader } from "../header/Header";
 
 export const Home = ({ themeClass, isDarkMode }) => {
-  const [isUserList, setIsUserList] = useState(null);
-  const [onlineUserData, setOnlineUserData] = useState(null);
-  const userToken = localStorage.getItem('access_token');
+  const [isCovidData, setIsCovidData] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const itemsPerPage = 15;
 
   useEffect(() => {
-    const getUserList = async () => {
-      await axios.get('http://localhost:8000/user/user-list', {
-        headers: {
-          "Authorization": `Bearer ${userToken}`
-        }
-      })
+    const covidList = async () => {
+      setIsLoading(true);
+      await axios.get(`https://corona.lmao.ninja/v2/countries?page=${currentPage}&limit=${itemsPerPage}`)
         .then((res) => {
           const data = res.data;
-          setIsUserList(data)
+          setIsCovidData(data);
+          setIsLoading(false);
+          setTotalPages(res.headers.get('X-Pagination-Page-Count'));
         })
     };
 
-    const getOnlineUserList = async () => {
-      await axios.get('http://localhost:8000/user/online-user-list', {
-        headers: {
-          "Authorization": `Bearer ${userToken}`
-        }
-      })
-        .then((res) => {
-          const data = res.data;
-          setOnlineUserData(data);
-          console.log(res.data, 'online users')
-        });
-    };
-
-    getOnlineUserList();
-    getUserList();
-  }, [])
+    covidList();
+  }, [currentPage])
 
   return (
     <div className={`w-full h-full bg-grey-50 ${themeClass}`}>
       <MainHeader themeClass={themeClass} isDarkMode={isDarkMode} />
-
       <div className="px-10 flex justify-between">
-        <div className="pt-10">
-          {isUserList && <div className={`max-w-sm p-6 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ${isDarkMode ? 'bg-grey-800 border-gray-700' : 'bg-white'}`}>
-            <GroupIcon style={{ fontSize: '35px' }} className={`${isDarkMode ? 'text-white' : 'text-grey-900'}`} />
-            <a href="#">
-              <h5 className={`mb-2 text-2xl font-semibold tracking-tight ${isDarkMode ? 'text-white' : 'text-gray-900'}`}>Need a help in Claim?</h5>
-            </a>
-            <p className={`mb-3 font-normal text-gray-500 dark:text-gray-400 ${isDarkMode ? 'text-white' : 'text-grey-900'}`}>Total Users: {isUserList?.length}</p>
-          </div>}
-          {!isUserList && (
-            <div role="status" className="max-w-sm animate-pulse">
-              <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
-              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
-              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
-              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+        <div className="pt-10 w-full h-full relative overflow-x-auto shadow-md sm:rounded-lg">
+          {isLoading && <div role="status">
+            <div className="flex flex-col items-center justify-center h-screen w-screen">
+              <svg aria-hidden="true" className="w-12 h-12 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+              </svg>
               <span className="sr-only">Loading...</span>
             </div>
-          )}
-        </div>
+          </div>}
+          {!isLoading && <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Country
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Population
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Cases
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Recovered
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Deaths
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Active
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {isCovidData && isCovidData?.map((item, index) => {
+                return (
+                  <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700" key={index}>
+                    <th scope="row" className="flex items-center px-3 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                      {/* <img src={item?.countryInfo?.flag} alt='No Img' style={{ width: "50px", height: "50px", borderRadius: "50px" }} /> */}
+                      <span className="px-2">{item?.country}</span>
+                    </th>
+                    <td className="px-6 py-4">
+                      {item?.population}
+                    </td>
+                    <td className="px-6 py-4">
+                      {item?.cases}
+                    </td>
+                    <td className="px-6 py-4">
+                      {item?.recovered}
+                    </td>
+                    <td className="px-6 py-4">
+                      {item?.deaths}
+                    </td>
+                    <td className="px-6 py-4">
+                      {item?.active}
+                    </td>
+                    <td className="px-6 py-4">
+                      <Link to={`/${item.country}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Detail</Link>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>}
+          {!isLoading && <button
+            disabled={currentPage === 1}
+            onClick={() => setCurrentPage(currentPage - 1)}
+          >
+            Previous Page
+          </button>}
 
-        <div className="pt-10">
-          <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-
-            <div className="flex items-center justify-between mb-4">
-              {onlineUserData && <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Online Customers</h5>}
-              {!onlineUserData && (<div role="status" className="max-w-sm animate-pulse">
-                <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-                <span className="sr-only">Loading...</span>
-              </div>)}
-            </div>
-
-            <div className="flow-root">
-              <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
-
-                {!onlineUserData && (
-                  <div role="status" className="max-w-sm animate-pulse">
-                    <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
-                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
-                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
-                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
-                    <span className="sr-only">Loading...</span>
-                  </div>
-                )}
-
-                {onlineUserData && onlineUserData?.map((item, index) => {
-                  return (
-                    <li className="py-3 sm:py-4" key={index}>
-                      <div className="flex items-center space-x-4">
-                        <div className="flex-shrink-0">
-                        </div>
-                        <div className="flex-1 min-w-0">
-                          <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                            {item?.name}
-                          </p>
-                          <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                            {item?.email}
-                          </p>
-                        </div>
-                        <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                          {item?.online && <span>online</span>}
-                        </div>
-                      </div>
-                    </li>
-                  )
-                })}
-              </ul>
-            </div>
-
-          </div>
+          {!isLoading && <button
+            disabled={currentPage >= totalPages}
+            onClick={() => setCurrentPage(currentPage + 1)}
+          >
+            Next Page
+          </button>}
         </div>
       </div>
     </div>
