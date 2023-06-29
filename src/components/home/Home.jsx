07f19bab53c984f44a0a670aaa80a229d4d8c798
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-29 17:38:22 +0400

    update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -73,7 +73,7 @@ export const Home = () => {
               <span className="sr-only">Loading...</span>
             </div>
           </div>}
-          {!isLoading && <table className="w-full h-screen text-sm text-left text-gray-500 dark:text-gray-400">
+          {!isLoading && <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
               <tr>
                 <th scope="col" className="px-6 py-3">
